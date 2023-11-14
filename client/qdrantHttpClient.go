1854fcd40da9ce62ea804def8462b875cc80b441
lei.gui <550686030@qq.com>
2023-11-14 09:38:29 +0800

    Use pointer replace value transfer.

--- client/qdrantHttpClient.go
+++ client/qdrantHttpClient.go
@@ -3,7 +3,6 @@ package client
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io"
 	"net/http"
 	"strconv"
@@ -25,29 +24,25 @@ func CreateHttpclient(schema string, hostname string, port int) *QDrantHttpClien
 	}
 }
 
-func (client *QDrantHttpClient) GetPoints(collectionName string, request point.PointsStringGetRequest) point.PointsGetResponse {
+func (client *QDrantHttpClient) GetPoints(collectionName string, request *point.PointsStringGetRequest) point.PointsGetResponse {
 	bytesData, _ := json.Marshal(request)
 	url := client.Scheme + "://" + client.HostName + ":" + strconv.Itoa(client.Port) + "/collections/" + collectionName + "/points"
-	fmt.Printf("url: %v\n", url)
 	resp, _ := http.Post(url, "application/json", bytes.NewReader(bytesData))
 	body, _ := io.ReadAll(resp.Body)
-	fmt.Printf("string(body): %v\n", string(body))
 	response := &point.PointsGetResponse{}
 	json.Unmarshal(body, response)
 	return *response
 }
 
-func (client *QDrantHttpClient) UpsertPoints(collectionName string, request point.PointsListUpsertRequest) point.UpdateResultResponse {
+func (client *QDrantHttpClient) UpsertPoints(collectionName string, request *point.PointsListUpsertRequest) point.UpdateResultResponse {
 	bytesData, _ := json.Marshal(request)
 	url := client.Scheme + "://" + client.HostName + ":" + strconv.Itoa(client.Port) + "/collections/" + collectionName + "/points"
-	fmt.Printf("url: %v\n", url)
 	req, _ := http.NewRequest("PUT", url, bytes.NewBuffer(bytesData))
 	req.Header.Set("Content-Type", "application/json")
 	httpClient := &http.Client{}
 	resp, _ := httpClient.Do(req)
 	body, _ := io.ReadAll(resp.Body)
 	defer resp.Body.Close()
-	fmt.Printf("string(body): %v\n", string(body))
 	response := &point.UpdateResultResponse{}
 	json.Unmarshal(body, response)
 	return *response
